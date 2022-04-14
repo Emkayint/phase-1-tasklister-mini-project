936af1b204d9a2a6d554450d64b54cfaad879958
Martin <mkmartiones@gmail.com>
2022-04-14 09:20:36 +0300

    Finished Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let totalTasks = [];
+  function myTask(taskDescription){
+    this.task = taskDescription;
+  }
+
+  let myform = document.getElementById("create-task-form")
+  myform.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let description = document.getElementById("new-task-description").value;
+    
+    if(description !== ''){
+      let taskObj = new myTask(description)
+      totalTasks.push(taskObj)
+      appendElem(totalTasks)
+      e.target.reset()
+    } else {
+      alert("Hey You Havent entered anything")
+    }
+  })  
 });
+
+function appendElem(elem){
+  let p = document.createElement('p')
+  let btn = document.createElement("button")
+  btn.textContent = " x "
+  let toAppend = document.getElementById("tasks")
+  
+  elem.forEach( mytask => {
+    p.textContent = ` ${mytask.task} `;
+    p.appendChild(btn);
+    toAppend.appendChild(p)
+    
+  })
+  btn.addEventListener("click", deleteToDo)
+}
+
+function deleteToDo(e){
+  e.target.parentNode.remove()
+}
